4684414f11ba0d4c93b02ae53f9459c67f85cab4
Lothar TheQuiet <lotharthequiet@gmail.com>
2022-10-10 09:10:15 -0400

    Lothar added netcat.py

---
+++ Chapter2/netcat.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import argparse
+import shlex
+import socket
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    cmd = cmd.strip()
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
